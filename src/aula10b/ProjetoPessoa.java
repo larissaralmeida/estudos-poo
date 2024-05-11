58acd945288eb7446215c3472b238178ac187e6b
Larissa <larissaralmeida.94@gmail.com>
2024-05-11 13:00:12 -0300

    ajustar instância

--- src/aula10b/ProjetoPessoa.java
+++ src/aula10b/ProjetoPessoa.java
@@ -3,28 +3,34 @@ package aula10b;
 public class ProjetoPessoa {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
-        Pessoa aluno = new Aluno();
-        Pessoa professor = new Professor();
-        Pessoa funcionario = new Funcionario();
+        Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+        Funcionario funcionario = new Funcionario();
 
         pessoa.setNome("Larissa");
         pessoa.setIdade(23);
         pessoa.setGenero("Feminino");
-        System.out.println(pessoa.detalhesPessoa());
 
         aluno.setNome("Lais");
         aluno.setIdade(20);
         aluno.setGenero("Feminino");
-        System.out.println(aluno.detalhesPessoa());
+        aluno.setCurso("Ciência da Computação");
 
         professor.setNome("Professor Gustavo Guanabara");
         professor.setIdade(35);
         professor.setGenero("Masculino");
-        System.out.println(professor.detalhesPessoa());
+        professor.setEspecialidade("Professor de Programação");
 
         funcionario.setNome("Lulu");
         funcionario.setIdade(29);
         funcionario.setGenero("Feminino");
+        funcionario.setTrabalhando(true);
+
+
+        System.out.println(pessoa.detalhesPessoa());
+        System.out.println(aluno.detalhesPessoa());
+        System.out.println(professor.detalhesPessoa());
         System.out.println(funcionario.detalhesPessoa());
+
     }
 }
